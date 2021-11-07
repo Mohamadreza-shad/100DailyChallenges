80f9bbaa8ca5c1a0cb7bcc94ec95a266f62f7958
RezaShad <mohamadrezashad1994@gmail.com>
2021-11-07 14:51:40 +0330

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _100DailyChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hi Sir");
+        }
+    }
+}
